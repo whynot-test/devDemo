417b4a729ed98031699acb7835499db16a7953ba
bijunjian <bijunjian@thunisoft.com>
2024-03-11 09:00:43 +0800

    新建项目

---
+++ src/main/java/com/example/devdemo/DevDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.devdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devdemo/DevDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
